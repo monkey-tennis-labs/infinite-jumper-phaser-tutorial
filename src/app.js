e77b4c6212b10f590e6167c07e843ce6237c3ea7
PetarSimonovic <petar@simonovic.com>
2023-06-14 20:57:54 +0100

    Platforms and player added

--- src/app.js
+++ src/app.js
@@ -1,134 +1,51 @@
 import Phaser from "phaser";
-import sky from "./assets/sky.png";
-import ground from "./assets/platform.png";
-import star from "./assets/star.png";
-import bomb from "./assets/bomb.png";
-import dude from "./assets/dude.png";
+import bg_layer1 from "/assets/bg_layer1.png";
+import groundGrass from "../assets/ground_grass.png";
+import bunnyStand from "../assets/bunny1_stand.png";
 
-class Example extends Phaser.Scene {
-  scoreText;
-  score = 0;
-  cursors;
-  platforms;
-  stars;
-  player;
-
+class Game extends Phaser.Scene {
   preload() {
-    this.load.image("sky", sky);
-    this.load.image("ground", ground);
-    this.load.image("star", star);
-    this.load.image("bomb", bomb);
-    this.load.spritesheet("dude", dude, {
-      frameWidth: 32,
-      frameHeight: 48,
-    });
+    this.load.image("background", bg_layer1);
+    this.load.image("platform", groundGrass);
+    this.load.image("bunny-stand", bunnyStand);
   }
 
   create() {
-    this.add.image(400, 300, "sky");
+    this.add.image(240, 320, "background");
+    const platforms = this.physics.add.staticGroup();
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
 
-    this.platforms = this.physics.add.staticGroup();
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = platforms.create(x, y, "platform");
+      platform.scale = 0.5;
 
-    this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
-
-    this.platforms.create(600, 400, "ground");
-    this.platforms.create(50, 250, "ground");
-    this.platforms.create(750, 220, "ground");
-
-    this.player = this.physics.add.sprite(100, 450, "dude");
-
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: "turn",
-      frames: [{ key: "dude", frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.stars = this.physics.add.group({
-      key: "star",
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 },
-    });
-
-    this.stars.children.iterate((child) => {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
-
-    this.scoreText = this.add.text(16, 16, "score: 0", {
-      fontSize: "32px",
-      fill: "#000",
-    });
-
-    this.physics.add.collider(this.player, this.platforms);
-    this.physics.add.collider(this.stars, this.platforms);
-
-    this.physics.add.overlap(
-      this.player,
-      this.stars,
-      this.collectStar,
-      null,
-      this
-    );
-  }
-
-  update() {
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160);
-
-      this.player.anims.play("left", true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160);
-
-      this.player.anims.play("right", true);
-    } else {
-      this.player.setVelocityX(0);
-
-      this.player.anims.play("turn");
+      /** @type {Phaser.Physics.Arcade.StaticBody} */
+      const body = platform.body;
+      body.updateFromGameObject();
     }
 
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
+    this.player = this.physics.add
+      .sprite(240, 320, "bunny-stand")
+      .setScale(0.5);
   }
 
-  collectStar(player, star) {
-    star.disableBody(true, true);
-
-    this.score += 10;
-    this.scoreText.setText(`Score: ${this.score}`);
-  }
+  update() {}
 }
 
-const config = {
+export default new Phaser.Game({
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 480,
+  height: 640,
+  scene: Game,
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 300 },
-      debug: false,
+      gravity: {
+        y: 200,
+      },
+      debug: true,
     },
   },
-  scene: Example,
-};
-
-new Phaser.Game(config);
+});
