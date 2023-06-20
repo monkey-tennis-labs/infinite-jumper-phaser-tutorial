2830d00a8b7f33156b5673d8e9b8a6156e0c908c
PetarSimonovic <petar@simonovic.com>
2023-06-20 21:00:16 +0100

    Horizontal movement; carrots

--- src/app.js
+++ src/app.js
@@ -2,15 +2,23 @@ import Phaser from "phaser";
 import bg_layer1 from "/assets/bg_layer1.png";
 import groundGrass from "../assets/ground_grass.png";
 import bunnyStand from "../assets/bunny1_stand.png";
+import carrot from "../assets/carrot.png";
+import Carrot from "./Carrot";
 
 class Game extends Phaser.Scene {
   /** @type {Phaser.Physics.Arcade.StaticGroup} */
   platforms;
+  /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+  cursors;
+  /** @type {Phaser.Physics.Arcade.Group} */
+  carrots;
 
   preload() {
     this.load.image("background", bg_layer1);
     this.load.image("platform", groundGrass);
     this.load.image("bunny-stand", bunnyStand);
+    this.load.image("carrot", carrot);
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   create() {
@@ -40,6 +48,19 @@ class Game extends Phaser.Scene {
     this.physics.add.collider(this.platforms, this.player);
 
     this.cameras.main.startFollow(this.player);
+
+    this.cameras.main.setDeadzone(this.scale.width * 1.5);
+
+    this.carrots = this.physics.add.group({ classType: Carrot });
+    this.physics.add.collider(this.platforms, this.carrots);
+
+    this.physics.add.overlap(
+      this.player,
+      this.carrots,
+      this.handleCollectCarrot,
+      undefined,
+      this
+    );
   }
 
   update() {
@@ -51,13 +72,59 @@ class Game extends Phaser.Scene {
       if (platform.y >= scrollY + 700) {
         platform.y = scrollY - Phaser.Math.Between(50, 100);
         platform.body.updateFromGameObject();
+        this.addCarrotAbove(platform);
       }
     });
+
     const touchingDown = this.player.body.touching.down;
 
     if (touchingDown) {
       this.player.setVelocityY(-300);
     }
+
+    if (this.cursors.left.isDown && !touchingDown) {
+      this.player.setVelocityX(-200);
+    } else if (this.cursors.right.isDown && !touchingDown) {
+      this.player.setVelocityX(200);
+    } else {
+      this.player.setVelocityX(0);
+    }
+
+    this.horizontalWrap(this.player);
+  }
+
+  /**
+   * @param {Phaser.GameObjects.Sprite} sprite
+   */
+  horizontalWrap(sprite) {
+    const halfWidth = sprite.displayWidth * 0.5;
+    const gameWidth = this.scale.width;
+    if (sprite.x < -halfWidth) {
+      sprite.x = gameWidth + halfWidth;
+    } else if (sprite.x > gameWidth + halfWidth) {
+      sprite.x = -halfWidth;
+    }
+  }
+
+  /**
+   * @param {Phaser.GameObjects.Sprite} sprite
+   */
+  addCarrotAbove(sprite) {
+    const y = sprite.y - sprite.displayHeight;
+
+    /** @type {Phaser.Physics.Arcade.Sprite} */
+    const carrot = this.carrots.get(sprite.x, y, "carrot");
+    carrot.setActive(true);
+    carrot.setVisible(true);
+    this.add.existing(carrot);
+    carrot.body.setSize(carrot.width, carrot.height);
+
+    return carrot;
+  }
+
+  handleCollectCarrot(player, carrot) {
+    this.carrots.killAndHide(carrot);
+    this.physics.world.disableBody(carrot.body);
   }
 }
 
